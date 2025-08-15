2d912199f1f66e4a8ca33b7c11c3d21786e14305
Kosalram Ramakrishnan <92807367+kosalram-tonyshive@users.noreply.github.com>
2025-08-15 16:42:06 +0200

    feat: add horizontally scrollable portfolio section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/about";
 import Footer from "@/components/footer";
 import Hero from "@/components/hero";
+import Portfolio from "@/components/portfolio";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <main>
         <Hero />
         <About />
+        <Portfolio />
       </main>
       <Footer />
     </>
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ import About from "./about";
 import Footer from "./footer";
 import Hero from "./hero";
 import Nav from "./navbar";
+import Portfolio from "./portfolio";
 import ThreeLogo from "./threelogo";
 
-export { About, Hero, Nav, ThreeLogo, Footer };
+export { About, Footer, Hero, Nav, Portfolio, ThreeLogo };
