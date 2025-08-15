cc386368589f2f28fab7e99be9785d350dffeb78
Kosalram Ramakrishnan <92807367+kosalram-tonyshive@users.noreply.github.com>
2025-08-15 17:03:28 +0200

    feat: lighten theme and sticky header

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,9 +2,9 @@ import About from "./about";
 import Footer from "./footer";
 import Hero from "./hero";
 import Nav from "./navbar";
-import Testimonials from "./testimonials";
 import Portfolio from "./portfolio";
-import ThreeLogo from "./threelogo";
 import Services from "./services";
+import Testimonials from "./testimonials";
+import ThreeLogo from "./threelogo";
 
 export { About, Footer, Hero, Nav, Portfolio, ThreeLogo, Services, Testimonials };
